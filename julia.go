82344f5712a784ff74cc42d0f653855ad4d19c3e
Teodor-15 <marthagilmore65@outlook.com>
2024-10-21 01:14:39 +0000

    Update project documentation, including the README file and other materials.

---
+++ julia.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func GenerateJuliaSet(c complex128, maxIter int) (image.Image, error) {
+	const (
+		width, height = 800, 800
+		zoom          = 1.0
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			z := complex(float64(x)/zoom-zoom/2, float64(y)/zoom-zoom/2)
+			iter := 0
+
+			for cmplx.Abs(z) < 2 && iter < maxIter {
+				z = z*z + c
+				iter++
+			}
+ // I’m not sure how this works, but it does.
+			colorValue := uint8(255 * iter / maxIter)
+			img.Set(x, y, color.RGBA{colorValue, 0, 0, 255})
+		}
+	}
+
+	return img, nil
+}
+
---
+++ sierpinski.go
@@ -0,0 +1,84 @@
+package main
+
+
+
+import (
+
+	"image"
+
+	"image/color"
+
+)
+
+
+
+func GenerateSierpinskiTriangle(size int) (image.Image, error) {
+
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
+
+
+
+	// Fill the background with black
+
+	for x := 0; x < size; x++ {
+
+		for y := 0; y < size; y++ {
+ // 12
+			img.Set(x, y, color.Black)
+
+		}
+
+	}
+
+
+
+	// Draw the Sierpinski triangle
+
+	drawSierpinski(img, 0, 0, size)
+
+
+
+	return img, nil
+
+}
+
+
+
+func drawSierpinski(img *image.RGBA, x, y, size int) {
+
+	if size < 1 {
+
+		return
+
+	}
+
+
+
+	// Draw the triangle
+ // 28
+	for i := 0; i < size; i++ {
+
+		for j := 0; j <= i; j++ {
+
+			img.Set(x+j, y+i, color.White)
+
+			img.Set(x+size-i+j, y+i, color.White)
+
+		}
+
+	}
+
+
+
+	// Recursively draw the smaller triangles
+
+	drawSierpinski(img, x, y, size/2)
+
+	drawSierpinski(img, x+size/2, y, size/2)
+
+	drawSierpinski(img, x+size/4, y+size/2, size/2)
+
+}
+
+
+
