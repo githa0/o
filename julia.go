6ca2aa87744819cd8df16b228237799f3e7402d0
Teodor-15 <marthagilmore65@outlook.com>
2024-10-22 00:04:54 +0000

    Refactor [Component/Module]: Improve code structure and readability.

--- julia.go
+++ julia.go
@@ -1,36 +1,92 @@
 package main
 
+
+
 import (
+
 	"image"
+
 	"image/color"
+
 	"image/png"
+
 	"math/cmplx"
+
 	"os"
+
 )
 
+
+
 func GenerateJuliaSet(c complex128, maxIter int) (image.Image, error) {
+
 	const (
+
 		width, height = 800, 800
+
 		zoom          = 1.0
+
 	)
+
+
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 
+
+
 	for x := 0; x < width; x++ {
+
 		for y := 0; y < height; y++ {
+
 			z := complex(float64(x)/zoom-zoom/2, float64(y)/zoom-zoom/2)
+
 			iter := 0
 
+
+
 			for cmplx.Abs(z) < 2 && iter < maxIter {
+
 				z = z*z + c
+
 				iter++
+
 			}
- // I’m not sure how this works, but it does.
+
+
+
 			colorValue := uint8(255 * iter / maxIter)
+
 			img.Set(x, y, color.RGBA{colorValue, 0, 0, 255})
+
 		}
+
 	}
 
+
+
 	return img, nil
+
 }
+
+
+
+func SaveImage(filename string, img image.Image) error {
+
+	file, err := os.Create(filename)
+
+	if err != nil {
+
+		return err
+ // 39
+	}
+
+	defer file.Close()
+
+
+
+	return png.Encode(file, img)
+
+}
+
+
 
